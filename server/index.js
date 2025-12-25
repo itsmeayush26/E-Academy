a66a51203822c42c3b7712ae876ad85df46fd409
itsmeayush26 <ayushsingh0255@gmail.com>
2025-12-25 13:29:51 +0530

    update API endpoints to use localhost for development

--- server/index.js
+++ server/index.js
@@ -22,21 +22,12 @@ const app=express();
 
 const PORT =process.env.PORT || 3000;
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://learn-academy-ecwl.vercel.app",
-];
 
+app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (Postman, mobile apps)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin:"http://localhost:5173",
     credentials: true,
   })
 );
